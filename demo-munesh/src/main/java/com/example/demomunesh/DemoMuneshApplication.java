e4eebf38304621966caf173823d3ded266b08f1e
Munesh <immuneshsingh@gmail.com>
2022-11-25 22:18:13 +0530

    my first commit

---
+++ demo-munesh/src/main/java/com/example/demomunesh/DemoMuneshApplication.java
@@ -0,0 +1,13 @@
+package com.example.demomunesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMuneshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMuneshApplication.class, args);
+	}
+
+}
